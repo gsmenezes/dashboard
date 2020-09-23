b4cb101ea98adbe4db252f8fa8f4f5f1f282258a
Gilda Menezes <menezes.s.gilda@gmail.com>
2020-09-23 17:03:52 -0300

    create filters and add new layout

---
+++ src/Components/HistoryCard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Tag } from './styles';
+
+interface IHistoryCardProps {
+    tagColor: string;
+    title: string;
+    subtitle: string;
+    amount: string;
+}
+
+const HistoryCard: React.FC<IHistoryCardProps> = ({tagColor, title, subtitle, amount}) => {
+    return (
+        <Container>
+            <Tag color={tagColor} />
+            <div>
+                <span>{title}</span>
+                <small>{subtitle}</small>
+            </div>
+            <h3>{amount}</h3>
+        </Container>
+    );
+}
+
+export default HistoryCard;
--- src/Pages/List/index.tsx
+++ src/Pages/List/index.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
-import ContentHeader from '../../Components/Content/Header';
-import SelectInput from '../../Components/SelectInput';
-import { Container } from './styles';
+import React from "react";
+import ContentHeader from "../../Components/Content/Header";
+import HistoryCard from "../../Components/HistoryCard";
+import SelectInput from "../../Components/SelectInput";
+import { Container, Content, Filters } from "./styles";
 
 const List: React.FC = () => {
-    const options = [{
-        value:'' , label: ''
-      }]
-    
-      return (
-        <Container>
-          <ContentHeader title="Saídas" lineColor="#E44C4E">
-            <SelectInput options={options} />
-            </ContentHeader>
-        </Container>
-      );
-    };
+  const months = [
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+  ];
+
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 },
+    { value: 2018, label: 2018 },
+  ];
+
+  return (
+    <Container>
+      <ContentHeader title="Saídas" lineColor="#E44C4E">
+        <SelectInput options={months} />
+        <SelectInput options={years} />
+      </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter recurrent">
+          Recorrentes{" "}
+        </button>
+        <button type="button" className="tag-filter eventual">
+          Eventuais{" "}
+        </button>
+      </Filters>
+
+      <Content>
+        <HistoryCard
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="23/09/2020"
+          amount="R$ 130,00"
+        />
+      </Content>
+    </Container>
+  );
+};
 
 export default List;
