2f25155510d7e632853465b7e6beaf19a648c623
gi menezes <54290429+gsmenezes@users.noreply.github.com>
2020-10-15 03:06:08 -0300

    finalizes project
    create animation

--- src/Components/HistoryCard/styles.ts
+++ src/Components/HistoryCard/styles.ts
@@ -1,8 +1,23 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 interface ITagProps {
     color: string;
 }
+
+const animate = keyframes `
+0% {
+    transform: translateX(-100px);
+    opacity:0;
+}
+50%{
+    opacity: .3;
+}
+100%{
+    transform:translateX(0px);
+    opacity: 1;
+}
+`;
+
 
 export const Container = styled.li`
 background-color: ${props => props.theme.colors.tertiary};
@@ -16,6 +31,8 @@ align-items:center;
 cursor: pointer;
 transition: all .3s;
 position: relative;
+
+animation: ${animate} .5s ease-in-out;
 
 &:hover{
     opacity: .7;
